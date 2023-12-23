ae8ec8c45a04983aa86d3274b4685c962e962fd6
Chandravo <chandravob2003@gmail.com>
2023-12-24 01:27:07 +0530

    updated asgi

--- mikelegal/asgi.py
+++ mikelegal/asgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from app.routing import websocket_urlpatterns
 from django.conf import settings
@@ -18,8 +20,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mikelegal.settings')
 
 
 # application = get_asgi_application()
-django.setup()
-django_asgi_app = get_asgi_application()
+# django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
