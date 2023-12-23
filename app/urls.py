ee39cd92c8071ab103f25d88e6200f47dff153d2
Chandravo <chandravob2003@gmail.com>
2023-12-23 13:47:39 +0530

    added index page for better ux

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import re_path,path
 from . import views
 urlpatterns = [
+    path('', views.index, name='index'),
     path('info/<str:key>/', views.home, name='home'),
+    path('createRoom/', views.createRoom, name='createRoom'),
+    path('joinRoom/', views.joinRoom, name='joinRoom'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+
+import random
+import string
 # Create your views here.
 def home(request, key):
     messages = Message.objects.filter(room__key=key).order_by('created_at')
@@ -9,7 +12,21 @@ def home(request, key):
         # print(message.message)
     return render(request, 'chatroom.html',{'key':key, 'text':text})
 
+def createRoom(request):
+    key = ''.join(random.choice(string.ascii_letters) for i in range(6))
+    while (ChatRoom.objects.filter(key=key).exists()):
+        key = ''.join(random.choice(string.ascii_letters) for i in range(6))
+    room = ChatRoom.objects.create(key=key)
+    room.save()
+    return redirect('/info/'+key)
+
+def joinRoom(request):
+    key = request.POST['key']
+    if (ChatRoom.objects.filter(key=key).exists()):
+        return redirect('/info/'+key)
+    else:
+        return render(request, 'index.html', {'error':'Room does not exist'})
 
 def index(request):
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', context = {'error':''})
