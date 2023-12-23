8f100db7ca15fc0e8519da4b393a39eac4cef0f7
Chandravo <chandravob2003@gmail.com>
2023-12-23 13:18:08 +0530

    chat history loaded

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request, key):
-    
-    return render(request, 'chatroom.html',{'key':key})
+    messages = Message.objects.filter(room__key=key).order_by('created_at')
+    text = ""
+    for message in messages:
+        text+= "Anonymous: "+message.message+"\n"
+        # print(message.message)
+    return render(request, 'chatroom.html',{'key':key, 'text':text})
 
 
 def index(request):
